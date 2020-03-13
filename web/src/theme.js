73247c103e4ceb02eea6e6a1d454d16ec32ce52a
Lachlan Campbell <lachlan@hackclub.com>
2020-03-13 14:30:21 -0400

    Add fonts to theme

--- web/src/theme.js
+++ web/src/theme.js
@@ -1,3 +1,6 @@
+const fonts = `system-ui, -apple-system, BlinkMacSystemFont,
+'Segoe UI', 'Roboto', 'Helvetica Neue', Helvetica, sans-serif`
+
 const theme = {
   colors: {
     violet: 'rgb(82, 72, 156)',
@@ -6,6 +9,10 @@ const theme = {
     snow: 'rgb(235, 235, 235)',
     red: 'rgb(244, 91, 105)',
   },
+  fonts: {
+    sans: `'Gotham Rounded SSm A', 'Gotham Rounded SSm B', ${fonts}`,
+    serif: `Sentinel, ${fonts}`
+  }
 }
 
 export default theme
