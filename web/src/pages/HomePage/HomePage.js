0aac099fea72faa771cd88619560bcb678ce5e12
Lachlan Campbell <lachlan@hackclub.com>
2020-03-13 02:33:35 -0400

    Begin scaffolding frontend

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -3,14 +3,44 @@ const HomePage = () => {
     <main>
       <h1>COVID-19 Tracker</h1>
       <p>By @zachlatta + @lachlanjc</p>
+      <section>
+        <aside>
+          <h2>Countries</h2>
+        </aside>
+        <article>
+          Chart goes here
+        </article>
+      </section>
       <style jsx>{`
         main {
-          max-width: 48rem;
+          max-width: 64rem;
           margin: 2rem auto;
-          text-align: center;
+          padding: 0 1rem;
         }
         h1 {
           font-size: 3rem;
+          margin-bottom: 0;
+        }
+        h1 + p {
+          margin-top: 0.5rem;
+          margin-bottom: 3rem;
+        }
+        section {
+          display: grid;
+          grid-gap: 2rem;
+        }
+        aside h2 {
+          margin-top: 0;
+        }
+        article {
+          border: 2px dashed #ddd;
+          border-radius: 1rem;
+          padding: 2rem;
+        }
+        @media (min-width: 48em) {
+          section {
+            grid-template-columns: 1fr 2fr;
+          }
         }
       `}</style>
     </main>
