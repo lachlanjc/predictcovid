094fcf3007c92865f00d54ca27e5cfffb9b07100
Lachlan Campbell <lachlan@hackclub.com>
2020-03-13 02:20:23 -0400

    Use styled-jsx

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -3,6 +3,16 @@ const HomePage = () => {
     <main>
       <h1>COVID-19 Tracker</h1>
       <p>By @zachlatta + @lachlanjc</p>
+      <style jsx>{`
+        main {
+          max-width: 48rem;
+          margin: 2rem auto;
+          text-align: center;
+        }
+        h1 {
+          font-size: 3rem;
+        }
+      `}</style>
     </main>
   )
 }
