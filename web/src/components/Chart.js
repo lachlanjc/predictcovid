18aa5fd6854369004e43e47acf368626492cc54f
Lachlan Campbell <lachlan@hackclub.com>
2020-03-15 16:06:15 -0400

    Add x axis ticks

--- web/src/components/Chart.js
+++ web/src/components/Chart.js
@@ -8,11 +8,13 @@ const yAxisFormatter = (i) =>
     .toString()
     .replace(/0{6}$/, 'M')
     .replace(/0{3}$/, 'k')
+const xAxisFormatter = (t) =>
+  new Date(t).toLocaleDateString().replace(/\/?2020-?/, '')
 
 const Chart = ({ dailyCounts, enabledCountries, defaultCountry, log }) => {
   return (
     <BarChart width={768} height={512} data={dailyCounts}>
-      <XAxis dataKey="name" />
+      <XAxis dataKey="date.date" tickFormatter={xAxisFormatter} />
       <YAxis tickFormatter={yAxisFormatter} scale={log ? 'sqrt' : 'linear'} />
       <Tooltip
         separator=": "
