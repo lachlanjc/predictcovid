2dad746e784554cbf3cc9b5d277f3d616b3335b7
Lachlan Campbell <lachlan@hackclub.com>
2020-03-15 16:01:33 -0400

    Stack chart by date

--- web/src/components/Chart.js
+++ web/src/components/Chart.js
@@ -3,23 +3,34 @@ import { startCase } from 'lodash'
 import commaNumber from 'comma-number'
 import theme from 'src/theme'
 
+const yAxisFormatter = (i) =>
+  i
+    .toString()
+    .replace(/0{6}$/, 'M')
+    .replace(/0{3}$/, 'k')
+
 const Chart = ({ dailyCounts, enabledCountries, defaultCountry, log }) => {
   return (
     <BarChart width={768} height={512} data={dailyCounts}>
       <XAxis dataKey="name" />
-      <YAxis
-        tickFormatter={(i) => i.toString().replace(/0{6}$/, 'M').replace(/0{3}$/, 'k')}
-        scale={log ? 'sqrt' : 'linear'}
-      />
+      <YAxis tickFormatter={yAxisFormatter} scale={log ? 'sqrt' : 'linear'} />
       <Tooltip
         separator=": "
         formatter={(value, name) => [commaNumber(value), startCase(name)]}
       />
       <Legend formatter={startCase} />
-      <Bar dataKey="newDeaths" fill={theme.colors.red} />
-      <Bar dataKey="newCases" fill={theme.colors.cyan} />
-      <Bar dataKey="currentlyInfected" fill={theme.colors.blue} />
-      <Bar dataKey="totalCases" fill={theme.colors.violet} />
+      <Bar dataKey="newDeaths" stackId="date.date" fill={theme.colors.red} />
+      <Bar dataKey="newCases" stackId="date.date" fill={theme.colors.cyan} />
+      <Bar
+        dataKey="currentlyInfected"
+        stackId="date.date"
+        fill={theme.colors.blue}
+      />
+      <Bar
+        dataKey="totalCases"
+        stackId="date.date"
+        fill={theme.colors.violet}
+      />
     </BarChart>
   )
 }
