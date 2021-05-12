89b8f1a173752a1be7768a952ebf1adfaaf3b8fb
bozdoz <howaboutben@gmail.com>
2021-05-12 01:35:23 -0300

    removes accidental scaffold css

--- web/src/App.js
+++ web/src/App.js
@@ -4,7 +4,6 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
-import './scaffold.css'
 import './index.css'
 
 const App = () => (
