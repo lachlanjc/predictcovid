e24eacce352333c70070276cb76ecfdf1c8ccdcf
bozdoz <howaboutben@gmail.com>
2021-05-12 23:37:16 -0300

    better scraper (gets xAxis and series data directly); better typing; less logs; proper returning statusCodes

--- api/src/functions/scrape.js
+++ api/src/functions/scrape.js
@@ -1,33 +1,43 @@
 // TODO DB lock when scraping is running
 import fetch from 'isomorphic-unfetch'
-import cheerio from 'cheerio'
-import { db } from 'src/lib/db'
+import cheerio, { html } from 'cheerio'
+import { db } from '../lib/db'
 
 // data returns in format: { "2020-02-28": 324, "2020-02-29": 500 }
 const extractDataFromChart = function($, chartId) {
+  const chartParent = $(`div#${chartId}`).parent()
   /** @type string */
-  const htmlAttempt = $(`div#${chartId}`).parent().parent().parent().parent().find('script').html();
+  // sometimes it's the parent, sometimes parent * 4
+  const htmlAttempt = chartParent.find('script').html() || chartParent.parent().parent().parent().find('script').html();;
 
-  const hackedChartJSON = htmlAttempt.split('Highcharts.chart(').find(
-      (chart) => chart.includes(chartId)
-    )
-    .replace(/^.*?\{/, '{')
-    .replace(/\}\);[\s\n]*?$/gi, '}')
-    .replace(/([A-z]+):/g, '"$1":')
-    .replace(/'/g, '"')
+  const hackedChartJSON = htmlAttempt.split('Highcharts.chart(')[1];
 
-  const json = JSON.parse(hackedChartJSON)
+  const xAxisCategories = hackedChartJSON.match(/xAxis:.*?{.*?categories\:.*?(\[.*?\])/s)
+
+  if (!xAxisCategories) {
+    console.log('cannot find xAxis categories', hackedChartJSON.substr(hackedChartJSON.indexOf('xAxis'), 200));
+
+    throw new Error('cannot get xAxis for', chartId)
+  }
+
+  const xVals = JSON.parse(xAxisCategories[1])
+
+  const seriesData = hackedChartJSON.match(/series:.*?data:.*?(\[.*?\])/s)
+
+  if (!seriesData) {
+    console.log('cannot find series data', hackedChartJSON.substr(hackedChartJSON.indexOf('series'), 200));
+
+    throw new Error('cannot get series data for', chartId)
+  }
+
+  const yVals = JSON.parse(seriesData[1])
 
   console.log("parsed chart json!")
-
-  const xVals = json.xAxis.categories
-  const yVals = json.series[0].data
 
   let data = {}
 
   xVals.forEach((val, i) => {
-    // need to manually set year to 2020
-    let day = new Date(val + ", 2020")
+    const day = new Date(val)
 
     // converts date from "Feb 24" to YYYY-MM-DD
     const fmtedDay = day.toISOString().split('T')[0]
@@ -93,17 +103,13 @@ const dbBS = async function() {
 
     console.log('\nBeginning sync with DB...')
     for (const day in data) {
-      console.log(
-        `Fetching Day for ${day} (and creating one if it doesn't exist)`
-      )
+      const date = new Date(day);
       const dbDay = await db.day.upsert({
-        update: { date: new Date(day) },
-        where: { date: new Date(day) },
-        create: { date: new Date(day) }
+        update: { date: date },
+        where: { date: date },
+        create: { date: date }
       })
 
-      console.log(`
-Beginning DailyCount creation for ${day} (or update if it already exists)...`)
       const foundCounts = await db.dailyCount.findMany({
         where: {
           country: { id: country.id },
@@ -111,15 +117,18 @@ Beginning DailyCount creation for ${day} (or update if it already exists)...`)
         }
       })
 
-      if (foundCounts.length == 0) {
-        console.log('No DailyCount found. Creating one.')
-        const {
-          totalCases,
-          newCases,
-          currentlyInfected,
-          totalDeaths,
-          newDeaths
-        } = data[day]
+      const {
+        totalCases,
+        newCases,
+        currentlyInfected,
+        totalDeaths,
+        newDeaths
+      } = data[day]
+
+      if (foundCounts.length === 0) {
+        console.log(
+          `Adding ${country.name} ${day}`
+        )
         await db.dailyCount.create({
           data: {
             country: { connect: { id: country.id } },
@@ -131,15 +140,10 @@ Beginning DailyCount creation for ${day} (or update if it already exists)...`)
             newDeaths
           }
         })
-      } else if (foundCounts.length == 1) {
-        console.log(`DailyCount #${foundCounts[0].id} found. Updating it...`)
-        const {
-          totalCases,
-          newCases,
-          currentlyInfected,
-          totalDeaths,
-          newDeaths
-        } = data[day]
+      } else if (foundCounts.length === 1) {
+        console.log(
+          `Updating ${country.name} ${day}`
+        )
         await db.dailyCount.update({
           where: { id: foundCounts[0].id },
           data: {
@@ -159,13 +163,20 @@ Beginning DailyCount creation for ${day} (or update if it already exists)...`)
   }
 }
 
-export const handler = (event, context, callback) =>
-  dbBS()
-    .then(() => {
-      console.log('success')
-      return callback(null, { status: 200, body: 'Success!' })
-    })
-    .catch((err) => {
-      console.error(err)
-      return callback(null, { status: 500, body: 'Error! ' + err })
-    })
+export const handler = async (event, context) => {
+  try {
+    await dbBS()
+    console.log('success')
+    return {
+      statusCode: 200,
+      body: JSON.stringify('Success!')
+    }
+  } catch (e) {
+    console.error(e)
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify('Error! ' + e)
+    }
+  }
+}
